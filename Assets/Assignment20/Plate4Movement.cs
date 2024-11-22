bf97545588b33bfb68c671859abdc265f4889402
tasneem qanoa <simora2017@gmail.com>
2024-11-22 15:11:39 +0200

    first commit

---
+++ Assets/Assignment20/Plate4Movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate4Movement : MonoBehaviour
+{
+    float x = 0.1f;
+    float y = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(x, 0, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-x, 0, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, y, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -y, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            transform.position = Vector3.zero;
+        }
+    }
+}
